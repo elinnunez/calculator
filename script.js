409a8964e3d6e89b0b3a6999e1238d2dc8da87cd
Elinnoel Nunez <elinnoelnunez@outlook.com>
2021-12-26 22:19:09 -0600

    todo: fix when choosing operator before numbers

--- script.js
+++ script.js
@@ -33,9 +33,12 @@ const operate = (num1, operator, num2) => {
   }
 };
 
-// const dispNum1;
-// const operator;
-// const dispNum2;
+let dispNum1 = "";
+let operator;
+let dispNum2 = "";
+let result = 0;
+
+let isOperatorChosen = false;
 
 const calctainer = document.querySelector(".calculator");
 const calcscreen = document.querySelector(".screen");
@@ -45,24 +48,48 @@ const buttons = document.querySelectorAll("button");
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
-    console.log(typeof e.target.value);
-    if(!isNaN(e.target.value)) {
-      calcscreen.textContent = `${e.target.value}`;
+    // console.log(e.target.value);
+    calcscreen.textContent = "";
+    if (!isNaN(e.target.value)) {
+      if (isOperatorChosen == false) {
+        dispNum1 += e.target.value;
+        console.log("Number 1: " + dispNum1);
+        calcscreen.textContent = `${dispNum1}`;
+      } else {
+        dispNum2 += e.target.value;
+        console.log("Number 2: " + dispNum2);
+        calcscreen.textContent = `${dispNum2}`;
+      }
     } else {
-      alert(e.target.value);
+      // console.log(typeof e.target.value);
+      if (e.target.value == "=") {
+        if (dispNum1 != "" && dispNum2 != "") {
+          result = operate(parseInt(dispNum1), operator, parseInt(dispNum2));
+          // console.log(typeof result);
+          calcscreen.textContent = `${result}`;
+          dispNum1 = result.toString();
+          dispNum2 = "";
+          isOperatorChosen = false;
+        } else {
+          calcscreen.textContent = "";
+        }
+      } else if (e.target.value == "all-clear") {
+        dispNum1 = "";
+        operator;
+        dispNum2 = "";
+        result = 0;
+        console.log("Cur Operator: " + operator);
+        isOperatorChosen = false;
+      } else {
+        operator = e.target.value;
+        console.log("Cur Operator: " + e.target.value);
+      }
     }
-  })
-})
+  });
+});
 
-const displayVal = 99999999999;
+const displayVal = 0;
 const maxDisplaySize = 11;
 // alert(maxDisplaySize);
 
 calcscreen.textContent = `${displayVal}`;
-
-// console.log(add(1,3));
-// console.log(subtract(1,3));
-// console.log(multiply(2,3));
-// console.log(divide(1,3));
-
-// console.log(operate(1, "+", 3));
